023c2de8adf105f26eee46c3712cc45f04611ea2
Joakim Stien <joakim.stien@gmail.com>
2017-04-05 21:51:47 +0200

    SFML and LiquidFun seems to link and run OK

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
-#include <Box2D/Box2D.h>
-#include <stdio.h>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	printf("lols\n");
-	return 0;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+    return EXIT_SUCCESS;
 }
